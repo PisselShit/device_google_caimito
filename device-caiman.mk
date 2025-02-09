db6e4d29107ee1c58293f63a4835d616cc8e4b9d
Pranav Vashi <neobuddy89@gmail.com>
2025-02-09 22:35:54 +0530

    caimito: Add hide cutout emulations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device-caiman.mk
+++ device-caiman.mk
@@ -138,6 +138,14 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
 
+# Set support hide display cutout feature
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_hide_display_cutout=true
+
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay \
+    AvoidAppsInCutoutOverlay
+
 # VINTF
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/vintf/device_framework_matrix_product.xml
